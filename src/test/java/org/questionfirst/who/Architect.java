6ab15b56eccd3c45e2d9e40ddd89b0ab861af3a3
Marko Umek <umek.marko@gmail.com>
2018-03-17 10:04:13 +0100

    Bio, Intentions on BigBoss.

--- src/test/java/org/questionfirst/who/Architect.java
+++ src/test/java/org/questionfirst/who/Architect.java
@@ -19,25 +19,7 @@
 package org.questionfirst.who;
 
 /**
- * BigBoss is responsible for ...
+ * Developers is responsible for ...
  */
-public class BigBoss implements Stakeholder {
-    @Override
-    public String bio() {
-        return "I'm the Big Boss and I'm the best ever lived on earth. Working at leat 48 h the day. And my biggest buddy is D. Trump.";
-    }
-
-    @Override
-    public Intentions[] intentions() {
-        return new Intentions[] { //
-            new BigBoss.AsCheapAsPossible(), //
-            new BigBoss.DeliveredYesterday(), //
-        };
-    }
-
-    private class AsCheapAsPossible implements Intentions {
-    }
-
-    private class DeliveredYesterday implements Intentions {
-    }
+public abstract class Architect implements Stakeholder {
 }
--- src/test/java/org/questionfirst/who/Users.java
+++ src/test/java/org/questionfirst/who/Users.java
@@ -21,23 +21,6 @@ package org.questionfirst.who;
 /**
  * BigBoss is responsible for ...
  */
-public class BigBoss implements Stakeholder {
-    @Override
-    public String bio() {
-        return "I'm the Big Boss and I'm the best ever lived on earth. Working at leat 48 h the day. And my biggest buddy is D. Trump.";
-    }
+public abstract class Users implements Stakeholder {
 
-    @Override
-    public Intentions[] intentions() {
-        return new Intentions[] { //
-            new BigBoss.AsCheapAsPossible(), //
-            new BigBoss.DeliveredYesterday(), //
-        };
-    }
-
-    private class AsCheapAsPossible implements Intentions {
-    }
-
-    private class DeliveredYesterday implements Intentions {
-    }
 }
